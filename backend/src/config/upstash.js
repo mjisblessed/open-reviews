4411ab320a9b915254e0b7388744e59030942071
mjisblessed <mithasjanbade@gmail.com>
2025-07-23 15:34:19 +0530

    moving upstash cause it was idle

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -1,13 +1,13 @@
-import {Ratelimit} from "@upstash/ratelimit";
-import {Redis} from "@upstash/redis";
-import dotenv from "dotenv";
-dotenv.config();
-// This file configures Upstash Redis for rate limiting in the application.
+// import {Ratelimit} from "@upstash/ratelimit";
+// import {Redis} from "@upstash/redis";
+// import dotenv from "dotenv";
+// dotenv.config();
+// // This file configures Upstash Redis for rate limiting in the application.
 
-const rateLimit = new Ratelimit({
-  redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(10, "10 s"),
-  analytics: true
-});
+// const rateLimit = new Ratelimit({
+//   redis: Redis.fromEnv(),
+//   limiter: Ratelimit.slidingWindow(10, "10 s"),
+//   analytics: true
+// });
 
-export default rateLimit;
+// export default rateLimit;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import notesRoutes from './routes/notesRoutes.js'; 
 import { connectDB } from './config/db.js'; 
 import dotenv from 'dotenv';
-import rateLimiter from './middleware/rateLimiter.js';
+// import rateLimiter from './middleware/rateLimiter.js';
 import cors from 'cors'
 dotenv.config();
 
@@ -14,7 +14,7 @@ app.use(cors({
   origin: 'http://localhost:5173', // Adjust this to your frontend URL
 }));
 app.use(express.json()); // to get body data in JSON format
-app.use(rateLimiter)
+// app.use(rateLimiter)
 // app.use((req, res, next) => {
 //   console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
 //   next();
